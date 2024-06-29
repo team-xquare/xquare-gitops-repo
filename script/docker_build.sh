122d54a1d6ac68e05f94e7e0053534cd9264e4d4
HyunSu1768 <azxcv1768@gmail.com>
2024-06-29 19:53:59 +0900

    fix :: 스크립트 수정

--- script/docker_build.sh
+++ script/docker_build.sh
@@ -8,8 +8,6 @@ RANDOM_TAG=$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 8 | head -n 1)
 IMAGE_TAG="${ENVIRONMENT}-${RANDOM_TAG}"
 IMAGE_REGISTRY="786584124104.dkr.ecr.ap-northeast-2.amazonaws.com"
 
-whoami
-
 docker build -t "${IMAGE_REGISTRY}/${REPO_NAME}:${IMAGE_TAG}" .
 
 docker push "${IMAGE_REGISTRY}/${REPO_NAME}:${IMAGE_TAG}"
--- script/onpremise_agent_setting.sh
+++ script/onpremise_agent_setting.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
 
-sudo install -m 0755 -d /etc/apt/keyrings
-curl https://download.gocd.org/GOCD-GPG-KEY.asc | sudo gpg --dearmor -o /etc/apt/keyrings/gocd.gpg
-sudo chmod a+r /etc/apt/keyrings/gocd.gpg
-echo "deb [signed-by=/etc/apt/keyrings/gocd.gpg] https://download.gocd.org /" | sudo tee /etc/apt/sources.list.d/gocd.list
-sudo apt-get update
-sudo apt-get install go-agent
-
-echo "wrapper.app.parameter.100=-serverUrl" | sudo tee -a /usr/share/go-agent/wrapper-config/wrapper-properties.conf
-echo "wrapper.app.parameter.101=https://gocd.xquare.app/go" | sudo tee -a /usr/share/go-agent/wrapper-config/wrapper-properties.conf
-sudo systemctl restart go-agent
-
+# Docker Engine 설치
 sudo apt-get update
 sudo apt-get install ca-certificates curl gnupg
 sudo install -m 0755 -d /etc/apt/keyrings
@@ -30,3 +20,24 @@ sudo gpasswd -a $USER docker
 newgrp docker
 
 sudo usermod -aG docker go
+# 다른 그룹의 사용자도 접근할 수 있도록 권한을 변경
+sudo chmod 666 /var/run/docker.sock
+
+# aws-cli 설치
+sudo apt-get update
+sudo apt-get install awscli
+
+# gocd agent 설치
+sudo install -m 0755 -d /etc/apt/keyrings
+curl https://download.gocd.org/GOCD-GPG-KEY.asc | sudo gpg --dearmor -o /etc/apt/keyrings/gocd.gpg
+sudo chmod a+r /etc/apt/keyrings/gocd.gpg
+echo "deb [signed-by=/etc/apt/keyrings/gocd.gpg] https://download.gocd.org /" | sudo tee /etc/apt/sources.list.d/gocd.list
+sudo apt-get update
+sudo apt-get install go-agent
+
+echo "wrapper.app.parameter.100=-serverUrl" | sudo tee -a /usr/share/go-agent/wrapper-config/wrapper-properties.conf
+echo "wrapper.app.parameter.101=https://gocd.xquare.app/go" | sudo tee -a /usr/share/go-agent/wrapper-config/wrapper-properties.conf
+sudo systemctl restart go-agent
+
+
+
