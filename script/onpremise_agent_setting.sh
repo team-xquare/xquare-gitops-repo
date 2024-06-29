012fd4e0ac3b081143898f0c6e018705dcfdfa38
HyunSu1768 <azxcv1768@gmail.com>
2024-06-29 15:54:59 +0900

    refactor :: 스크립트 수정

--- script/onpremise_agent_setting.sh
+++ script/onpremise_agent_setting.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 
-sudo adduser --disabled-password --gecos "" go
-
-echo "go ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/go
-
-sudo -i -u go << 'EOF'
 sudo install -m 0755 -d /etc/apt/keyrings
 curl https://download.gocd.org/GOCD-GPG-KEY.asc | sudo gpg --dearmor -o /etc/apt/keyrings/gocd.gpg
 sudo chmod a+r /etc/apt/keyrings/gocd.gpg
 echo "deb [signed-by=/etc/apt/keyrings/gocd.gpg] https://download.gocd.org /" | sudo tee /etc/apt/sources.list.d/gocd.list
 sudo apt-get update
-sudo apt-get install -y go-agent
+sudo apt-get install go-agent
 
 echo "wrapper.app.parameter.100=-serverUrl" | sudo tee -a /usr/share/go-agent/wrapper-config/wrapper-properties.conf
 echo "wrapper.app.parameter.101=https://gocd.xquare.app/go" | sudo tee -a /usr/share/go-agent/wrapper-config/wrapper-properties.conf
@@ -24,4 +19,3 @@ sudo apt update
 sudo apt install -y docker-ce
 sudo usermod -aG docker $USER
 newgrp docker
-EOF
