43f13da2e84255ebdaedc2a4941c3fe2aff47b12
HyunSu1768 <azxcv1768@gmail.com>
2024-06-29 14:44:44 +0900

    feat :: template 변경

--- script/onpremise_agent_setting.sh
+++ script/onpremise_agent_setting.sh
@@ -22,6 +22,6 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o 
 echo "deb [arch=\$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \$(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt update
 sudo apt install -y docker-ce
-sudo usermod -aG docker \$USER
+sudo usermod -aG docker $USER
 newgrp docker
 EOF
