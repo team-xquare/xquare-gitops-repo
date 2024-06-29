b57093d4d0792bc0d5d62ae45419d5c5cbd79338
HyunSu1768 <azxcv1768@gmail.com>
2024-06-29 14:27:19 +0900

    feat :: script 변경

--- script/onpremise_agent_setting.sh
+++ script/onpremise_agent_setting.sh
@@ -1,19 +1,24 @@
-# gocd agent 설치
+#!/bin/bash
+
+sudo adduser --disabled-password --gecos "" go
+
+echo "go ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/go
+
+sudo -i -u go << 'EOF'
 sudo install -m 0755 -d /etc/apt/keyrings
 curl https://download.gocd.org/GOCD-GPG-KEY.asc | sudo gpg --dearmor -o /etc/apt/keyrings/gocd.gpg
 sudo chmod a+r /etc/apt/keyrings/gocd.gpg
 echo "deb [signed-by=/etc/apt/keyrings/gocd.gpg] https://download.gocd.org /" | sudo tee /etc/apt/sources.list.d/gocd.list
 sudo apt-get update
-sudo apt-get install go-agent
-wget https://download.gocd.org/binaries/24.2.0-19076/deb/go-agent_24.2.0-19076_all.deb
-sudo dpkg -i go-agent-24.2.0-19076.deb
+sudo apt-get install -y go-agent
 
-# Docker Engine 설치
+# Install Docker Engine
 sudo apt update
-sudo apt install apt-transport-https ca-certificates curl software-properties-common
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+echo "deb [arch=\$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \$(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt update
-sudo apt install docker-ce
-sudo usermod -aG docker $USER
+sudo apt install -y docker-ce
+sudo usermod -aG docker \$USER
 newgrp docker
+EOF
