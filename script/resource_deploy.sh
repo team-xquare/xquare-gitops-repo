1272bd0b8683a74244463c14d1a231e78dfa2a9d
HyunSu1768 <azxcv1768@gmail.com>
2024-06-29 22:07:09 +0900

    feat :: deploy 로직 수정

--- script/resource_deploy.sh
+++ script/resource_deploy.sh
@@ -4,16 +4,20 @@ SERVICE_NAME=$1
 ENVIRONMENT=$2
 REPOSITORY=$3
 
+git clone https://github.com/team-xquare/xquare-gitops-repo
+cd xquare-gitops-repo
+git checkout v2
+
 helm template \
   $SERVICE_NAME \
   templates/server \
   -f ./pipelines/$ENVIRONMENT/$SERVICE_NAME/values.yaml \
   --set repository=$REPOSITORY \
-  > ./pipelines/$ENVIRONMENT/$SERVICE_NAME/resource/manifest.yaml
+  > manifast.yaml
 
-kubectl apply -f ./pipelines/$ENVIRONMENT/$SERVICE_NAME/resource/manifest.yaml
+kubectl apply -f manifast.yaml
 
-git pull
-git add ./pipelines/$ENVIRONMENT/$SERVICE_NAME/resource/manifest.yaml
-git commit -m "record :: $SERVICE_NAME-$ENVIRONMENT kubernetes manifest"
-git push
+#git pull
+#git add ./pipelines/$ENVIRONMENT/$SERVICE_NAME/resource/manifest.yaml
+#git commit -m "record :: $SERVICE_NAME-$ENVIRONMENT kubernetes manifest"
+#git push
