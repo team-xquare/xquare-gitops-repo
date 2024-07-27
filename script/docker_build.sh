16cf8d27417fd5ba767552da32deb1f26bcb1a9f
HyunSu1768 <azxcv1768@gmail.com>
2024-07-27 22:39:44 +0900

    fix :: create ecr

--- script/docker_build.sh
+++ script/docker_build.sh
@@ -10,6 +10,15 @@ IMAGE_REGISTRY="786584124104.dkr.ecr.ap-northeast-2.amazonaws.com"
 
 ls
 
+REPO_EXISTS=$(aws ecr describe-repositories --repository-names "${REPO_NAME}" --region ap-northeast-2 2>&1 || echo "RepositoryNotFoundException")
+
+if [[ ${REPO_EXISTS} == *"RepositoryNotFoundException"* ]]; then
+  echo "Repository ${REPO_NAME} does not exist. Creating it now..."
+  aws ecr create-repository --repository-name "${REPO_NAME}" --region ap-northeast-2
+else
+  echo "Repository ${REPO_NAME} already exists."
+fi
+
 docker build -t "${IMAGE_REGISTRY}/${REPO_NAME}:${IMAGE_TAG}" .
 
 aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS --password-stdin ${IMAGE_REGISTRY}
