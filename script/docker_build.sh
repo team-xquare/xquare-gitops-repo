e64c314dfffabc234216271261450ddda586ffb6
HyunSu1768 <azxcv1768@gmail.com>
2024-07-27 23:16:38 +0900

    bug :: 버그 수정

--- script/docker_build.sh
+++ script/docker_build.sh
@@ -10,19 +10,25 @@ IMAGE_REGISTRY="786584124104.dkr.ecr.ap-northeast-2.amazonaws.com"
 
 ls
 
+# ECR 저장소가 존재하는지 확인
 REPO_EXISTS=$(aws ecr describe-repositories --repository-names "${REPO_NAME}" --region ap-northeast-2 2>&1 || echo "RepositoryNotFoundException")
 
 if [[ ${REPO_EXISTS} == *"RepositoryNotFoundException"* ]]; then
-  echo "Repository ${REPO_NAME} does not exist. Creating it now..."
+  echo "저장소 ${REPO_NAME}가 존재하지 않습니다. 지금 생성합니다..."
   aws ecr create-repository --repository-name "${REPO_NAME}" --region ap-northeast-2
 else
-  echo "Repository ${REPO_NAME} already exists."
+  echo "저장소 ${REPO_NAME}가 이미 존재합니다."
 fi
 
+# Docker 이미지를 빌드
 docker build -t "${IMAGE_REGISTRY}/${REPO_NAME}:${IMAGE_TAG}" .
 
+# AWS ECR에 로그인
 aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS --password-stdin ${IMAGE_REGISTRY}
 
+# Docker 이미지를 ECR 저장소에 푸시
 docker push "${IMAGE_REGISTRY}/${REPO_NAME}:${IMAGE_TAG}"
 
-echo "export ${SERVICE_NAME}-repository=${IMAGE_REGISTRY}/${REPO_NAME}:${IMAGE_TAG}" > build_result.env
+# 환경 변수 파일에 저장소 URI 저장
+VARIABLE_NAME=$(echo ${SERVICE_NAME}_REPOSITORY | tr '-' '_')
+echo "export ${VARIABLE_NAME}=${IMAGE_REGISTRY}/${REPO_NAME}:${IMAGE_TAG}" > build_result.env
--- script/resource_deploy.sh
+++ script/resource_deploy.sh
@@ -4,13 +4,16 @@ SERVICE_NAME=$1
 ENVIRONMENT=$2
 REPOSITORY=$3
 
+# GitOps 리포지토리로 이동하여 최신 변경 사항을 가져오기
 cd /home/go/xquare-gitops-repo && git pull
 
+# 리소스 디렉토리가 존재하는지 확인
 RESOURCE_DIR="./pipelines/$ENVIRONMENT/$SERVICE_NAME/resource"
 if [ ! -d "$RESOURCE_DIR" ]; then
   mkdir -p "$RESOURCE_DIR"
 fi
 
+# Helm을 사용하여 Kubernetes 매니페스트 생성
 helm template \
   $SERVICE_NAME \
   templates/server \
@@ -18,8 +21,10 @@ helm template \
   --set image_name=$REPOSITORY \
   > $RESOURCE_DIR/manifest.yaml
 
+# Kubernetes 매니페스트 적용
 kubectl apply -f $RESOURCE_DIR/manifest.yaml
 
+# 매니페스트를 Git 리포지토리에 커밋하고 푸시
 git pull
 git add $RESOURCE_DIR/manifest.yaml
 git commit -m "record :: $SERVICE_NAME-$ENVIRONMENT kubernetes manifest"
