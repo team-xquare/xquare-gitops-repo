cb47bf5db7cf8aa81c8d68eaf27f7cacc1e61bed
HyunSu1768 <azxcv1768@gmail.com>
2024-07-30 20:12:29 +0900

    fix :: pull 변경

--- script/resource_deploy.sh
+++ script/resource_deploy.sh
@@ -9,7 +9,7 @@ REPOSITORY=$3
 # GitOps 리포지토리로 이동하여 최신 변경 사항을 가져오기
 cd /home/go/xquare-gitops-repo
 git checkout v2
-git pull --rebase
+git pull
 
 # 리소스 디렉토리가 존재하는지 확인
 RESOURCE_DIR="./pipelines/$ENVIRONMENT/$SERVICE_NAME/resource"
@@ -30,6 +30,6 @@ kubectl apply -f $RESOURCE_DIR/manifest.yaml
 
 # 매니페스트를 Git 리포지토리에 커밋하고 푸시
 git add $RESOURCE_DIR/manifest.yaml
-git pull --rebase
+git pull
 git commit -m "record :: $SERVICE_NAME-$ENVIRONMENT kubernetes manifest"
 git push --set-upstream origin v2
