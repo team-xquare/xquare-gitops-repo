397f7b096ee57f51d049178a2cd57bc2d1ac3486
HyunSu1768 <azxcv1768@gmail.com>
2024-07-30 17:09:12 +0900

    fix :: pull 위치 변경

--- script/resource_deploy.sh
+++ script/resource_deploy.sh
@@ -29,7 +29,7 @@ helm template \
 kubectl apply -f $RESOURCE_DIR/manifest.yaml
 
 # 매니페스트를 Git 리포지토리에 커밋하고 푸시
-git pull --rebase
 git add $RESOURCE_DIR/manifest.yaml
+git pull --rebase
 git commit -m "record :: $SERVICE_NAME-$ENVIRONMENT kubernetes manifest"
 git push --set-upstream origin v2
